80c1c8b1de4376702cd68ef3f3194ca9dd5343ba
Prince Jaiswal <kmprince15932@gmail.com>
2023-07-03 00:16:29 +0530

    firstfunctiontest

--- stu_pannel/models.py
+++ stu_pannel/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 # Create your models here.
--- stu_pannel/views.py
+++ stu_pannel/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import datetime
+import time
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+from django.core.mail import message
+from django.shortcuts import render, get_list_or_404
+from django.http import HttpResponseRedirect, HttpResponse, request
+from  django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.urls import reverse
+from django.views import View
+from  django.urls import  reverse_lazy
+from django.views.generic.edit import CreateView
+from .decorators import login_required_with_autologout
 
-# Create your views here.
+
+def hello(request):
+    return render(request, "admin_pannel/Student_Bash.html", {})
